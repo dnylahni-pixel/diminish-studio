38553f8f5d4326c25e4ddcf184328de19b789850
dnylahni-pixel <dnylahni@gmail.com>
2026-06-08 15:29:59 +0330

    Update songs.ts

--- lib/db/src/schema/songs.ts
+++ lib/db/src/schema/songs.ts
@@ -9,7 +9,7 @@ import {
 } from "drizzle-orm/pg-core";
 import { artists } from "./artists";
 
-export const songsTable = pgTable("songs", {
+export const songs = pgTable("songs", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
 
